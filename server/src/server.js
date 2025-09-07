88a1c8ea691d49e9787356fdd44c520af3b7e1ab
rahulprasanna012 <rahulprasanna012@gmail.com>
2025-09-07 18:30:47 +0530

    error

--- server/src/server.js
+++ server/src/server.js
@@ -10,9 +10,13 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",   
-  credentials: true                  
+  origin: [
+    "http://ykssgogwsss0400k0gggs44s.31.97.230.12.sslip.io", 
+    "https://ykssgogwsss0400k0gggs44s.31.97.230.12.sslip.io" 
+  ],
+  credentials: true,
 }));
+
 
 app.use(cookieParser());
 app.use(express.json({ limit: '2mb' }));
